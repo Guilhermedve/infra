a8f33ebedf3c868616be4075e33b85ed8003cf03
guilhermedve <guilhermecontatoemp@gmail.com>
2026-07-09 20:02:00 -0300

    ikadu

--- datasources.tf
+++
@@ -1,2 +0,0 @@
-data "aws_s3_bucket" "bucket" {
-bucket = " ${var.bucket_Name}-bucket-${terraform.workspace}" } 
--- variables.tf
+++
@@ -1,5 +0,0 @@
-variable "bucket_Name" {
-  description = "The name of the S3 bucket to be created."
-  type        = string
-  default     = "treinoterraform-bucket"
-}
